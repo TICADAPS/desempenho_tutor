a85dd8d1313ec6c6d2888032f507a243f9a0c384
Marcelo Farias <marcelo.farias@adapsbrasil.com.br>
2024-04-30 15:18:55 -0300

    up desempenho_tutor

---
+++ recursos_online/api/v1/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('MYSQL_HOST',        'localhost');
+define('MYSQL_DATABASE',    'u226895969_desemp_tutor');
+define('MYSQL_USERNAME',    'u226895969_aval_tutor');
+define('MYSQL_PASSWORD',    'Senha10adaps');

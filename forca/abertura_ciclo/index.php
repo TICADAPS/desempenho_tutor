20eb6981b316bc9d475aecb70270701117616125
Marcelo Farias <marcelo.farias@adapsbrasil.com.br>
2025-01-03 16:47:36 -0300

    upaberturacicloindex

--- forca/abertura_ciclo/index.php
+++ forca/abertura_ciclo/index.php
@@ -403,6 +403,32 @@ $perfil = '3';
                                                     </div>
                                                   </div>
                                                 </div>
+                                            <?php }elseif($flagativo == 'SIM'){ 
+                                                     ?>   
+                                                    <td><button type="button" class="btn btn-danger shadow-sm border-white" data-toggle="modal" data-target="#modalFechar2<?= $id ?>">FECHAR</button></td><td></td>
+                                                    <!-- Modal modalFechar -->
+                                                    <div class="modal fade" id="modalFechar2<?= $id ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                      <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                          <div class="modal-header">
+                                                            <h5 class="modal-title" id="exampleModalLabel">FECHAMENTO DO CICLO</h5>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                              <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                          </div>
+                                                          <div class="modal-body">
+                                                              <h6>Deseja FECHAR o <?= $cicloA ?>º ciclo?
+                                                          </div>
+                                                          <div class="modal-footer">
+                                                           <form method="get" action="fechaciclo.php">
+                                                               <input type="hidden" value="<?= $id ?>" name="id">
+                                                               <button type="button" class="btn btn-secondary" data-dismiss="modal">NÃO</button>
+                                                               <button type="submit" class="btn btn-primary">FECHAR</button>
+                                                           </form>
+                                                          </div>
+                                                        </div>
+                                                      </div>
+                                                    </div>
                                             <?php }else{ ?>
                                             <td></td><td></td>
                                             <?php } ?>
